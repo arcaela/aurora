cd185335ed81267193bd4044b9a412d87632c172
Alejandro Reyes <arcaela.reyes@gmail.com>
2021-03-16 10:50:14 -0400

    CRA Update

--- cra-template-aurora/template/src/ServerLess/index.js
+++ cra-template-aurora/template/src/ServerLess/index.js
@@ -2,13 +2,8 @@ require('./configs/routes');
 
 module.exports.firebase = require('./configs/firebase').default;
 
-module.exports.error = require('./helpers/error').default;
-module.exports.getter = require('./helpers/getter').default;
-module.exports.Google = require('./helpers/Google').default;
 
-module.exports.api = require('./libs/api').default;
 module.exports.scopes = require('./libs/scopes').default;
-module.exports.default = module.exports.api;
-
 module.exports.useAuth = require('./hooks/useAuth').default;
-module.exports.useInputs = require('./hooks/useInputs').default;
+
+module.exports.default = module.exports.api;
