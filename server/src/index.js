2521cb3f26f52f6a6e519d65fd3f9b9ed84a563a
Alejandro Reyes <arcaela.reyes@gmail.com>
2021-03-17 13:33:15 -0400

    Server | Add Babel

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,5 @@
-module.exports.api = require('./api').default;
-module.exports.error = require('./error').default;
-module.exports.Google = require('./Google').default;
-module.exports.useGetter = require('./useGetter').default;
-module.exports.useInputs = require('./useInputs').default;
+module.exports.api = require('~/api').default;
+module.exports.error = require('~/error').default;
+module.exports.Google = require('~/Google').default;
+module.exports.useGetter = require('~/useGetter').default;
+module.exports.useInputs = require('~/useInputs').default;
