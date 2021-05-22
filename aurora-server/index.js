3fa182898cc612c8c95b5459135976dd0e81930f
Alejandro Reyes <arcaela.reyes@gmail.com>
2021-05-22 12:45:48 -0400

    Fix

--- aurora-server/index.js
+++ aurora-server/index.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 module.exports = require('./src/vendor/utils').CLI(require(
-    require('path').resolve(__dirname, (`app/commands/${process.argv[2]||'start'}`).replace(':','/'))
+    require('path').resolve(__dirname, (`src/app/commands/${process.argv[2]||'start'}`).replace(':','/'))
 ));
