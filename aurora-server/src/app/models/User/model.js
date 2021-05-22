b7b09d678085564010a9977147bece29b247f2d3
Alejandro Reyes <arcaela.reyes@gmail.com>
2021-05-22 12:40:36 -0400

    Model | User

--- aurora-server/src/app/models/User/model.js
+++ aurora-server/src/app/models/User/model.js
@@ -1,12 +1,8 @@
-const { Schema } = require("mongoose");
+/* const { Schema } = require("mongoose"); */
 /*
     Schemas Definition
     @ https://mongoosejs.com/docs/guide.html#definition
 */
-
 module.exports = {
-    role:{type:String,default:'user'},
-    uid:{type:Schema.Types.Mixed,unique:true},
-
 
 }
--- aurora-server/src/app/models/User/mutators.js
+++ aurora-server/src/app/models/User/mutators.js
@@ -5,14 +5,49 @@ const { Schema } = require("mongoose");
     @ https://mongoosejs.com/docs/middleware.html#pre
     @ https://mongoosejs.com/docs/middleware.html#post
 */
-module.exports = (schema=Schema.prototype)=>{
-    return schema.plugin(require('mongoose-role'),{
-        roles: ['admin','moderator','editor','user'],
-        accessLevels: {
-            admin: ['admin'],
-            moderator: ['admin','moderator'],
-            editor: ['admin','moderator','editor'],
-            user: ['admin','moderator','editor','user'],
-        }
+
+function hasPermissions(schema=Schema.prototype, permissions={}){
+    const _ = require("lodash");
+    const getKeys = (obj={}, index='', arr=[])=>{
+        return Object.entries(obj).reduce((_, [key,value])=>{
+                if(value && typeof value==='object' && !Array.isArray(value))
+                    getKeys(value, index?index+'.'+key:key, arr);
+                else if(value===true) arr.push(index?index+'.'+key:key);
+            return arr;
+        },[]);
+    }
+    const permissionsArray = (...allows)=>getKeys(_.merge(...allows));
+    schema.method('isPermission', function(...request){
+        return _.difference(permissionsArray(permissions,this.permissions), request.flat() ).length<1;
     });
+    schema.method('anyPermission', function(...request){
+        return _.intersection( permissionsArray(permissions,this.permissions), request.flat() ).length>0;
+    });
+}
+
+
+
+module.exports = (schema=Schema.prototype)=>{
+    
+    schema.plugin(hasPermissions , {
+        update:{
+            id:false,
+            uid:false,
+            email:true,
+            cedula:true,
+            displayName:true,
+            phoneNumber:true,
+            voting:{
+                departament:true,
+                municipality:true,
+                point:true,
+                table:true,
+            },
+            address:{
+                string:true,
+            }
+        },
+    });
+    
+
 };
