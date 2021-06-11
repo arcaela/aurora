ab2fa24d737582bfa7e17a5ff254ea89438cb039
Alejandro Reyes <arcaela.reyes@gmail.com>
2021-06-11 12:52:14 -0400

    Providers Init

--- aurora-server/src/app/providers/AppProvider.js
+++ aurora-server/src/app/providers/AppProvider.js
@@ -1,6 +1,15 @@
-module.exports = {
+const { Provider } = require('~/core');
+module.exports = class AppProvider extends Provider {
+    //  this.Aliasses : Aliasses Instance
+    //  this.Command : Command Instance
+    //  this.Logger : Logger Instance
+    //  this.Provider : This Provider class interface
+    //  this.Router : Express Router Instance
+    //  this.Socket : SocketIO Instance
+    //  this.Kernel : Kernel config options of Kernel File
+    //  this.PackageJSON : PackageJSON file in JSON Format
     /* Se llama el método boot apenas se instancia el modelo de datos */
-    boot(){
+    async boot(){
 
-    },
+    }
 };
--- aurora-server/src/app/providers/RouteProvider.js
+++ aurora-server/src/app/providers/RouteProvider.js
@@ -1,9 +1,12 @@
-const { scandir } = require("../../vendor");
-module.exports = {
+const { Provider } = require('~/core');
+module.exports = class RouteProvider extends Provider {
+    // server = null; Server Instance
+    // app = null; Express Router Instance
     /* Se llama el método boot apenas se instancia el modelo de datos */
-    boot(){ this.webRoutes(); },
-    /* Cargamos todos los archivos dentro de la carpeta /routes/web de manera recursiva */
-    webRoutes(){
-        scandir( __dirname, '../../routes/web' ).forEach(({path})=>require(path));
-    },
+    async boot(){ this.web(); }
+
+    web(){
+        this.Router.use((...[,,n])=>n(), ...this.Kernel.middlewares.map(fn=>(typeof fn==='string'?require(fn):fn)) )
+        require( aurora.paths.routes("web") );
+    }
 };
