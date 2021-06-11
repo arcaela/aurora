e86e31074157b396f969b01a63ec436f68b0a662
Alejandro Reyes <arcaela.reyes@gmail.com>
2021-06-11 12:50:26 -0400

    Remove the Vendors

--- aurora-server/src/vendor/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    socket:require('./socket'),
-    scandir:require('./scandir'),
-    utils:require('./utils'),
-    ucfirst:(str)=>str.toLowerCase().replace(/^([a-z\u00E0-\u00FC])|\s+([a-z\u00E0-\u00FC])/g,($1)=>$1.toUpperCase()),
-};
