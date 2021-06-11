7a43288a77ef9108b6b2a7d7eeb3da07ad415d68
Alejandro Reyes <arcaela.reyes@gmail.com>
2021-06-11 12:53:05 -0400

    Unused Middlewares

--- aurora-server/src/app/middlewares/Auth.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(req, res, next){
-    return req.auth?next():res.json({
-        ok:false,
-        error:null,
-        message:"Se requiere un inicio de sesión",
-        timestamp:Date.now(),
-    });
-}
--- aurora-server/src/app/middlewares/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    Auth:require('./Auth'),
-    FirebaseHeader:require('./FirebaseHeader'),
-    hasAccess:require('./hasAccess'),
-    Input:require('./Input'),
-    Response:require('./Response'),
-};
